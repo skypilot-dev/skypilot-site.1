6719016afebf902f58dd5a2682e398409f68217c
Camilla Thorsen <cthorsen@skypilot.dev>
2020-03-22 22:23:04 -0600

    Enable contact requests

---
+++ docs/lambda/send-email-to-skypilot.js
@@ -0,0 +1,36 @@
+const aws = require('aws-sdk');
+const ses = new aws.SES({ region: 'us-west-2' });
+
+exports.handler = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  const params = {
+    Destination: {
+      ToAddresses: [
+        'camilla@skypilot.dev',
+        'wthorsen@skypilot.dev',
+      ]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: JSON.stringify(body, undefined, 2),
+        }
+      },
+      Subject: {
+        Data: `Contact request from ${body.name} <${body.email}>`
+      }
+    },
+    Source: 'bot@skypilot.dev'
+  };
+
+  ses.sendEmail(params, function (err, data) {
+    callback(null, { err: err, data: data });
+    if (err) {
+      console.log(err);
+      context.fail(err);
+    } else {
+      console.log(data);
+      context.succeed(event);
+    }
+  });
+};
