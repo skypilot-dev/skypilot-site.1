b1e98a68d4452368036185daa0ea79cd755b5296
William Thorsen <william@thorsen.dev>
2020-09-23 23:13:15 -0600

    tooling: Upgrade Toolchain configs

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,11 @@ module.exports = {
     'js',
     'json',
   ],
-  testRegex: '__tests__/.*.test.ts$',
+  moduleNameMapper: {
+    'root/(.*)$': '<rootDir>/$1',
+    'src/(.*)$': '<rootDir>/src/$1',
+  },
+  testRegex: '__tests__/.*\\.test\\.ts$',
   /* Define preprocessors */
   transform: {
     '^.+\\.ts$': 'babel-jest',
